817c8e002795da8d6ce6a16da8f7e2c1366615d8
Anthony Kildery <90473131+anthonyKld@users.noreply.github.com>
2022-12-26 09:03:03 -0300

    .
    .

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-const robotron = document.getElementById('robotron')
+const controle = document.querySelectorAll('[data-controle]')   //arry
 
+controle.forEach((elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    })
+})
 
-robotron.addEventListener('click', ola)
-
-function ola() {
-    console.log('oi')
-    alert('oi')
+function manipulaDados(operacao, controle) {
+    const pesa = controle.querySelector('[data-contador]')
+    if(operacao === '-') {
+        pesa.value = parseInt(pesa.value) - 1
+    }else {
+        pesa.value = parseInt(pesa.value) + 1
+    }
 }
